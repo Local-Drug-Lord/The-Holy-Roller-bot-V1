3ed8cf97a66b0e0ebddc7f38ee6e97735e0e47c7
Local_Drug_Lord <Valpiswille@gmail.com>
2025-10-01 20:21:55 +0200

    Fixes https://github.com/Local-Drug-Lord/The-Holy-Roller-bot-V1/issues/41 and https://github.com/Local-Drug-Lord/The-Holy-Roller-bot-V1/issues/45 I also renamed some stuff to make it more clear Also remade SQL queries so they don't delete everything related to the thing (specific to /settings delete) Confirmation action still WIP and will soon be made

--- cogs/settings.py
+++ cogs/settings.py
@@ -29,11 +29,35 @@ async def log_entry(self, author_id, guild_id, What, Type, To):
     channel = await get_logging_channel(guild_id, self)
     if channel:
         image_file = File("Images/settings_icon.png", filename="settings_icon.png")
-        log_entry_embed = discord.Embed(title="Server config action!", description="Someone has made a changed a server config setting for The Holy Roller!", color=discord.Color.from_rgb(140,27,27))
+        log_entry_embed = discord.Embed(title="Server config action!", description="Someone has made a changed a server config for The Holy Roller!", color=discord.Color.from_rgb(140,27,27))
         log_entry_embed.add_field(name="", value=f"**{What}** **{Type}** was changed to **{To}**")
         log_entry_embed.set_thumbnail(url="attachment://settings_icon.png") 
         if Type == "attachment":
             log_entry_embed.set_image(url=To)
+        log_entry_embed.set_footer(text=f"Action made by: {author_name} ({author_id}).\nUTC: {current_time()}")
+        await channel.send(file=image_file, embed=log_entry_embed)
+    else:
+        return
+    
+async def delete_log_entry(self, author_id, guild_id, message_type, setting):
+    author_name = await self.bot.fetch_user(int(author_id))
+    channel = await get_logging_channel(guild_id, self)
+    if channel:
+        image_file = File("Images/settings_icon.png", filename="settings_icon.png")
+        log_entry_embed = discord.Embed(title="Server config action!", description="Someone has made a changed a server config for The Holy Roller!", color=discord.Color.from_rgb(140,27,27))
+        log_entry_embed.set_thumbnail(url="attachment://settings_icon.png") 
+        if message_type == None:
+            if setting == "All":
+                log_entry_embed.add_field(name="", value="**All** channel settings have been deleted for this server.")
+            else:
+                log_entry_embed.add_field(name="", value=f"**{setting}** channel setting have been deleted for this server.", inline=False)
+        else:
+            if setting == "All":
+                log_entry_embed.add_field(name="", value=f"**All {message_type}** message settings have been deleted for this server.")
+            elif message_type == "Attachment":
+                log_entry_embed.add_field(name="", value=f"**{message_type} {setting}** message setting have been deleted for this server.")
+            else:
+                log_entry_embed.add_field(name="", value=f"**{message_type} {setting}** message setting have been deleted for this server.")
         log_entry_embed.set_footer(text=f"Action made by: {author_name} ({author_id}).\nUTC: {current_time()}")
         await channel.send(file=image_file, embed=log_entry_embed)
     else:
@@ -498,30 +522,28 @@ class settings(commands.Cog):
         await ctx.send(embed=show_embed)
         return
 
-    #Delete
+    #delete
     @group.command(name="delete", description="Delete/reset a specific setting or all settings")
     @app_commands.checks.has_permissions(administrator=True)
-    async def delete(self, interaction: discord.Interaction, setting: typing.Literal["Logging/Logs", "Welcome", "Goodbye", "Prefix", "All"]):
+    async def delete(self, interaction: discord.Interaction, setting: typing.Literal["Logging/Logs", "Welcome_channel", "Goodbye_channel", "Prefix", "All"]):
         guild_id = interaction.guild.id
         author_id = interaction.user.id
-        await log_entry(self, author_id, guild_id, "Delete", "setting", setting)
+        await delete_log_entry(self, author_id, guild_id, None, setting)
         if setting == "Logging/Logs":
             await self.pool.execute('UPDATE info SET log_id = NULL WHERE guild_id = $1', guild_id)
             await interaction.response.send_message("Logging channel setting has been reset.")
         elif setting == "Welcome":
-            await self.pool.execute('UPDATE info SET wlc_id = NULL, wlc_pic = NULL, wlc_title = NULL, wlc_msg = NULL, wlc_rgb = NULL WHERE guild_id = $1', guild_id)
-            await interaction.response.send_message("Welcome settings have been reset.")
+            await self.pool.execute('UPDATE info SET wlc_id = NULL WHERE guild_id = $1', guild_id)
+            await interaction.response.send_message("Welcome channel setting have been reset.")
         elif setting == "Goodbye":
-            await self.pool.execute('UPDATE info SET bye_id = NULL, bye_pic = NULL, bye_title = NULL, bye_msg = NULL, bye_rgb = NULL WHERE guild_id = $1', guild_id)
-            await interaction.response.send_message("Goodbye settings have been reset.")
+            await self.pool.execute('UPDATE info SET bye_id = NULL WHERE guild_id = $1', guild_id)
+            await interaction.response.send_message("Goodbye channel setting have been reset.")
         elif setting == "Prefix":
             await self.pool.execute('UPDATE info SET prefix = $1 WHERE guild_id = $2', '!', guild_id)
-            await interaction.response.send_message('Prefix setting has been reset to !.')
+            await interaction.response.send_message('Prefix setting has been reset to default (!).')
         elif setting == "All":
-            await self.pool.execute('DELETE FROM info WHERE guild_id = $1', guild_id)
-            await interaction.response.send_message("All settings have been deleted for this server.")
-        else:
-            await interaction.response.send_message("Invalid setting.", ephemeral=True)
+            await self.pool.execute('UPDATE info SET log_id = NULL, wlc_id = NULL, bye_id = NULL, prefix = $1 WHERE guild_id = $2', '!', guild_id)
+            await interaction.response.send_message("All channel settings have been deleted for this server.")
         return
 
     #Prefix
@@ -531,22 +553,22 @@ class settings(commands.Cog):
         guild_id = ctx.guild.id
         author_id = ctx.author.id
         setting = setting.lower()
-        await log_entry(self, author_id, guild_id, "Delete", "setting", setting)
+        await delete_log_entry (self, author_id, guild_id, None, setting)
         if setting in {"logging", "logs", "log"}:
             await self.pool.execute('UPDATE info SET log_id = NULL WHERE guild_id = $1', guild_id)
             await ctx.send("Logging channel setting has been reset.")
         elif setting in {"welcome", "wlc"}:
-            await self.pool.execute('UPDATE info SET wlc_id = NULL, wlc_pic = NULL, wlc_title = NULL, wlc_msg = NULL, wlc_rgb = NULL WHERE guild_id = $1', guild_id)
-            await ctx.send("Welcome settings have been reset.")
+            await self.pool.execute('UPDATE info SET wlc_id = NULL WHERE guild_id = $1', guild_id)
+            await ctx.send("Welcome channel setting have been reset.")
         elif setting in {"goodbye", "bye"}:
-            await self.pool.execute('UPDATE info SET bye_id = NULL, bye_pic = NULL, bye_title = NULL, bye_msg = NULL, bye_rgb = NULL WHERE guild_id = $1', guild_id)
-            await ctx.send("Goodbye settings have been reset.")
+            await self.pool.execute('UPDATE info SET bye_id = NULL WHERE guild_id = $1', guild_id)
+            await ctx.send("Goodbye channel setting have been reset.")
         elif setting in {"prefix"}:
             await self.pool.execute('UPDATE info SET prefix = $1 WHERE guild_id = $2', '!', guild_id)
-            await ctx.send('Prefix setting has been reset to !.')
+            await ctx.send('Prefix setting has been reset to default (!).')
         elif setting == "all":
-            await self.pool.execute('DELETE FROM info WHERE guild_id = $1', guild_id)
-            await ctx.send("All settings have been deleted for this server.")
+            await self.pool.execute('UPDATE info SET log_id = NULL, wlc_id = NULL, bye_id = NULL, prefix = $1 WHERE guild_id = $2', '!', guild_id)
+            await ctx.send("All channel setting have been deleted for this server.")
         else:
             await ctx.send("Invalid setting. Use logging, welcome, goodbye, prefix, or all.")
         return
@@ -557,7 +579,7 @@ class settings(commands.Cog):
     async def delete_message(self, interaction: discord.Interaction, message_type: typing.Literal["Welcome", "Goodbye"], setting: typing.Literal["Attachment", "Title", "Message", "Color", "All"]):
         guild_id = interaction.guild.id
         author_id = interaction.user.id
-        await log_entry(self, author_id, guild_id, "Delete", "message_setting", f"{message_type} {setting}")
+        await delete_log_entry(self, author_id, guild_id, message_type, setting)
         if message_type == "Welcome":
             if setting == "Attachment":
                 await self.pool.execute('UPDATE info SET wlc_pic = NULL WHERE guild_id = $1', guild_id)
@@ -602,7 +624,7 @@ class settings(commands.Cog):
         author_id = ctx.author.id
         message_type = message_type.lower()
         setting = setting.lower()
-        await log_entry(self, author_id, guild_id, "Delete", "message_setting", f"{message_type} {setting}")
+        await delete_log_entry(self, author_id, guild_id, message_type, setting)
         if message_type in {"welcome", "wlc"}:
             if setting == "attachment":
                 await self.pool.execute('UPDATE info SET wlc_pic = NULL WHERE guild_id = $1', guild_id)
